5abe2593a416f4e512b24c6b7203728dea18127d
Abdul Rehman <mehars.6925@gmail.com>
2023-04-18 11:16:43 +0500

    implemented search feature

--- app/Http/Controllers/NewsFeed.php
+++ app/Http/Controllers/NewsFeed.php
@@ -2,14 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NewsFeed extends Controller
 {
     public function show(Request $request)
     {
-        $news = News::orderBy('publishedAt', 'DESC')->with('authors', 'sources')->paginate(15);
+        $searchQuery = $request->query('search');
+        $minDate = $request->query('minDate');
+        $maxDate = $request->query('maxDate');
+        $authors = $request->query('authors');
+        $sources = $request->query('sources');
+
+        $news = News::orderBy('publishedAt', 'DESC');
+        $news = $news->with('authors', 'sources');
+
+        if ($authors) {
+            $authors = json_decode($authors);
+            $news = $news->whereExists(function ($query) use ($authors) {
+                $query->select('*')
+                    ->from('news_authors')
+                    ->whereRaw('news_authors.news_id = news.id')
+                    ->whereIn('news_authors.author_id', $authors);
+            });
+        }
+
+        if ($sources) {
+            $sources = json_decode($sources);
+            $news = $news->whereExists(function ($query) use ($sources) {
+                $query->select('*')
+                    ->from('news_sources')
+                    ->whereRaw('news_sources.news_id = news.id')
+                    ->whereIn('news_sources.source_id', $sources);
+            });
+        }
+
+
+        if ($minDate) {
+            $news = $news->where('publishedAt', '>=', $minDate);
+        }
+
+        if ($maxDate) {
+            $news = $news->where('publishedAt', '<=', $maxDate);
+        }
+
+        if ($searchQuery) {
+            $news = $news->where('title', 'like', $searchQuery);
+            $news = $news->where('description', 'like', $searchQuery);
+        }
+
+        $news = $news->paginate(15);
 
         return response($news, 200);
     }
